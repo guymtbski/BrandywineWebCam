d08ea8a7d674dd50e8818f3b1d445c58c55f5fc6
guymtbski <guy_mcall@hotmail.com>
2024-11-01 18:49:53 -0700

    Update ConvertToVid.py

--- ConvertToVid.py
+++ ConvertToVid.py
@@ -31,6 +31,7 @@ def create_timelapse(image_folder, output_video_path, fps=4):
     print(f"Timelapse video created at: {output_video_path}")
 
 # Usage
-image_folder = '/'  # Change this to your image folder path
+image_folder = os.path.dirname(os.path.abspath(__file__))  # Change this to your image folder path
+
 output_video_path = 'timelapse_video.mp4'  # Desired output video file name
 create_timelapse(image_folder, output_video_path)
